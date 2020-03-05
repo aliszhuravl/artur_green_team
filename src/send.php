6834146fc03c0a00bcf49dc20c067b6a84c24304
Alisa Zhuravleva <alis.zhuravl@gmail.com>
2020-03-05 20:44:05 +0300

    Update send.php

--- src/send.php
+++ src/send.php
@@ -10,7 +10,7 @@ $email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("alis.zhuravl@icloud.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From:  \r\n"))
+if (mail("alis.zhuravl@icloud.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From:alis.zhuravl@icloud.com  \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
